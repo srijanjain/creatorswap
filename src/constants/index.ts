e45122b3f173b27503c88d14300438a38f69d009
Utsav Singla <utsav18321@iiitd.ac.in>
2021-12-07 00:08:01 +0530

    added 3 more tokens to list

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,11 +7,14 @@ type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
 
-export const MEM = new Token(ChainId.BSCTESTNET, '0x4B556A2A032511718b1C569a1a53dDbCB3B703af', 18, 'MEM', 'PancakeSwap Token')
+export const MEM = new Token(ChainId.BSCTESTNET, '0x4B556A2A032511718b1C569a1a53dDbCB3B703af', 18, 'MEM', 'Memboro Token')
 export const WBNB = new Token(ChainId.BSCTESTNET, '0x0dE8FCAE8421fc79B29adE9ffF97854a424Cad09', 18, 'WBNB', 'Wrapped BNB')
-export const ANJ = new Token(ChainId.BSCTESTNET, '0xe1662823d8044254409eB682fE34d35792677228', 18, 'ANJ', 'Dai Stablecoin')
-export const SANDY = new Token(ChainId.BSCTESTNET, '0x6dbbFBF5e5A291d6397B3dc2410d4257947288fF', 18, 'SANDY', 'Binance USD')
-export const UTSI = new Token(ChainId.BSCTESTNET, '0x9e174afba0efe8E1a54212F823fCbebB88C88698', 18, 'UTSI', 'Tether USD')
+export const ANJ = new Token(ChainId.BSCTESTNET, '0xe1662823d8044254409eB682fE34d35792677228', 18, 'ANJ', 'Anjaan Token')
+export const SANDY = new Token(ChainId.BSCTESTNET, '0x6dbbFBF5e5A291d6397B3dc2410d4257947288fF', 18, 'SANDY', 'SAndy coin')
+export const UTSI = new Token(ChainId.BSCTESTNET, '0x9e174afba0efe8E1a54212F823fCbebB88C88698', 18, 'UTSI', 'Utsav Token')
+export const KSM = new Token(ChainId.BSCTESTNET, '0x93C5Fd28a4309b3505e037ec62123418246df165', 18, 'KSM', 'KABIR Token')
+export const SHUBH = new Token(ChainId.BSCTESTNET, '0x4be160985F81942549C5e0A132E0e0ae7f64FEff', 18, 'SHUBH', 'Shubham coin')
+export const CHHOTA = new Token(ChainId.BSCTESTNET, '0x8efE4395DeC7f5A10a88934d8DDB5f7A2D34169D', 18, 'CHHOTA', 'CHOTTA ANSHUL coin')
 export const ETH = new Token(ChainId.BSCTESTNET, '0xE282a15DBad45e3131620C1b8AF85B7330Cb3b4B', 18, 'ETH', 'Binance-Peg Ethereum Token')
 
 const WETH_ONLY: ChainTokenList = {
@@ -22,7 +25,7 @@ const WETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.BSCTESTNET]: [...WETH_ONLY[ChainId.BSCTESTNET], ANJ, SANDY, UTSI, ETH],
+  [ChainId.BSCTESTNET]: [...WETH_ONLY[ChainId.BSCTESTNET], ANJ, SANDY, UTSI, ETH, KSM, SHUBH, CHHOTA],
 }
 
 /**
@@ -36,13 +39,13 @@ export const CUSTOM_BASES: { [chainId in ChainId]?: { [tokenAddress: string]: To
 // used for display in the default list when adding liquidity
 export const SUGGESTED_BASES: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.BSCTESTNET]: [...WETH_ONLY[ChainId.BSCTESTNET], ANJ, SANDY, UTSI],
+  [ChainId.BSCTESTNET]: [...WETH_ONLY[ChainId.BSCTESTNET], ANJ, SANDY, UTSI,KSM, SHUBH, CHHOTA],
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
 export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.BSCTESTNET]: [...WETH_ONLY[ChainId.BSCTESTNET], ANJ, SANDY, UTSI],
+  [ChainId.BSCTESTNET]: [...WETH_ONLY[ChainId.BSCTESTNET], ANJ, SANDY, UTSI, KSM, SHUBH, CHHOTA],
 }
 
 export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
